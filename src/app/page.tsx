a8ad167d81138a29e3cbaa97ffeb21317dfd0cab
Rinat85 <pacmanreal@gmail.com>
2024-09-06 15:10:37 +0500

    feat: add h2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8">
       <h1>Courses Edit</h1>
+      <h2>Courses title</h2>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
