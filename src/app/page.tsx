26e6af0302b49a9d91ac52596f5d4a8b4e54c00d
Rinat85 <pacmanreal@gmail.com>
2024-09-06 13:56:57 +0500

    feat: add h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8">
+      <h1>Courses Edit</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
