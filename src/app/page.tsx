d6a421f8434248f578c4ad67fd6e19cd76be6565
Rinat85 <pacmanreal@gmail.com>
2024-09-08 13:28:26 +0500

    feat: larger h1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,16 +4,11 @@ import { CreateCourseForm } from "@/features/courses-list/pub/create-course-form
 export default async function Home() {
   return (
     <main className="flex flex-col min-h-screen p-8">
-      <h1>Courses Edit</h1>
-      <h2>Added</h2>
+      <h1 className="text-3xl mb-2">Courses Edit</h1>
       <CreateCourseForm
         revalidatePagePath="/"
         className="max-w-[300px] mb-10"
       />
-      <p>List</p>
-      <ul>
-        <li>Test123</li>
-      </ul>
       <CoursesList revalidatePagePath="/" />
     </main>
   );
