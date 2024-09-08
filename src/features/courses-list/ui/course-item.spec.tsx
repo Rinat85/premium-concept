2442da7889e04fa11fc47a459677a9deae048d69
Rinat85 <pacmanreal@gmail.com>
2024-09-08 13:04:08 +0500

    chor: add sentry

--- src/features/courses-list/ui/course-item.spec.tsx
+++ src/features/courses-list/ui/course-item.spec.tsx
@@ -2,16 +2,21 @@ import { render, screen } from "@testing-library/react";
 import { CourseItem } from "./course-item";
 import userEvent from "@testing-library/user-event";
 
-describe('course item', () => {
-  it('should call delete callback', async () => {
+describe("course item", () => {
+  it("should call delete callback", async () => {
     const onDelete = jest.fn();
-    render(<CourseItem course={{
-      id: 'asdasdsad',
-      name: 'asdasdsadczxc',
-      description: 'adzxcxzcxcxc'
-    }} onDelete={onDelete} />);
+    render(
+      <CourseItem
+        course={{
+          id: "asdasdsad",
+          name: "asdasdsadczxc",
+          description: "adzxcxzcxcxc",
+        }}
+        onDelete={onDelete}
+      />,
+    );
 
-    await userEvent.click(screen.getByText('Delete'));
+    await userEvent.click(screen.getByText("Delete"));
 
     expect(onDelete).toHaveBeenCalled();
   });
